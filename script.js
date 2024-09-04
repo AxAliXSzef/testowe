641894d7c78172d3f0bc0a4ca25e7e4a52ed5608
AxAliXSzef <alanekszramka@gmail.com>
2024-09-04 22:58:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Wyświetlanie modalu
-    $('#successModal').modal('show');
-    // Reset formularza po wysłaniu
-    this.reset();
+document.addEventListener('DOMContentLoaded', function() {
+    // Scroll to contact section
+    document.getElementById('contactButton').addEventListener('click', function(e) {
+        e.preventDefault();
+        document.getElementById('kontakt').scrollIntoView({ behavior: 'smooth' });
+    });
+
+    // Handle form submission
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // Wyświetlanie modalu
+        $('#successModal').modal('show');
+        // Reset formularza po wysłaniu
+        this.reset();
+    });
 });
